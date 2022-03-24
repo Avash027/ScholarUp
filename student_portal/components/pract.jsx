75431f8b1b8697fc5860bde6f19b018bf8390fe0
Saswat Samal <saswat.sam786@gmail.com>
2022-03-24 23:28:02 +0530

    made the initial of react native,reactjs and server

---
+++ student_portal/components/pract.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const pract = () => {
+  return (
+    <View>
+      <Text>pract</Text>
+    </View>
+  );
+};
+
+export default pract;
+
+const styles = StyleSheet.create({});
