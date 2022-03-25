6dc3aeb569aed5687bee9e06b5556a609cddf3a0
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-03-25 11:11:35 +0530

    Initialized server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-console.log("Server work will start soon");
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+require("./config/db")();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//Middlewares
+app.use(cors());
+app.use(express.json({ limit: "15mb" }));
+
+//Routes
+app.use(`/test`, require("./routes/test"));
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`);
+});
